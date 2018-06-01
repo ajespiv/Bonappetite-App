e74aee94a0c01b1f0a9e990ef3b0d9c053614f1c
Alexis Espinoza <ajespiv@gmail.com>
2018-06-01 12:05:56 -0400

    Bon Appetite Last Copy

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('turbolinks:load', function() {
+
+
+  $('.modal-footer .btn-primary').click(function() {
+    $('#upload').submit();
+  });
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
-def current_user
-  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-def require_user
-  return true if current_user
+  def require_user
+    return true if current_user
 
-  redirect_to '/login' unless current_user
-end
+    return redirect_to root_path
+  end
 
 #
 
